7fbf656d3f8606c298b25953d160f62317666d03
arunkumar <arunganesh7070@gmail.com>
2025-02-17 22:00:34 +0530

    added styling

--- weather forecasting/Components/Weather.jsx
+++ weather forecasting/Components/Weather.jsx
@@ -1,10 +1,25 @@
 import React from 'react'
 import '../Css/style.css'
 
+import clearSky from '../src/assets/clear-sky.png';
+import cloudyDay from '../src/assets/cloudy-day.png';
+import rainyDay from '../src/assets/rainy-day.png';
+import searchIcon from '../src/assets/search.png';
+import snow from '../src/assets/snow.png';
+import sun from '../src/assets/sun.png';
+import windy from '../src/assets/windy.png';
+     
 const Weather = () => {
   return (
-    <div>
-      hello from Weather
+    <div className='container'>
+        <div className='input-container'>
+            <input type="text" placeholder='Search city'  className='cityname'/>
+            <div className='search-icon'>
+                <img src="/" alt="" />
+            </div>
+
+
+        </div>
     </div>
   )
 }
